829018d54e22922a9cadd51e5a06a0af565cf9b9
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-03-14 13:51:30 +0200

    json

---
+++ index.js
@@ -0,0 +1,9 @@
+
+'use strict';
+const _ = require('lodash');
+
+const output = _.camelCase('Foo Bar kukuku')
+console.log(output);
+console.log("kukkuu");
+console.log("taas jotain uutta");
+console.log("kukkuu");
