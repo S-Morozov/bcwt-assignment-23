69776d1511c9e5445ff999a4df91ade764bae66d
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-03-14 14:10:13 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
 'use strict';
-const _ = require('lodash');
 
-const output = _.camelCase('Foo Bar kukuku')
-console.log(output);
-console.log("kukkuu");
-console.log("taas jotain uutta");
-console.log("kukkuu");
+console.log('Hello world!');
