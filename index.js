73bec9987053c99aaba130eca984d51e6d22e95c
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-03-15 11:36:24 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 'use strict';
 
 const express = require('express')
+const {render} = require("pug");
 const app = express()
 const port = 3000
 app.use(express.static('public'))
+
+
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res)=> {
+   res.render('index')
+});
+
 
 app.get("/catinfo", (req, res) => {
     const cat = {
