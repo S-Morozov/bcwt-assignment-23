4b33a1856077da564bb76fa623859593bafb735f
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-04-17 11:21:20 +0300

    #commits

--- models/catModel.js
+++ models/catModel.js
@@ -21,3 +21,22 @@ const cats = [
 module.exports = {
   cats,
 };
+
+
+"use strict";
+const pool = require("../database/db");
+const promisePool = pool.promise();
+
+const getAllCats = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.query("SELECT * FROM wop_cat");
+    return rows;
+  } catch (e) {
+    console.error("error", e.message);
+  }
+};
+
+module.exports = {
+  getAllCats,
+};
